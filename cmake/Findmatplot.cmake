d92f25f94a21a52dcf6073b7f4f9eed488d22a4f
rojung <roland.jung@aau.at>
2023-08-08 15:09:32 +0200

    [REFA] unused cmake files removed + Findmatplot updated

--- cmake/Findmatplot.cmake
+++ cmake/Findmatplot.cmake
@@ -1,42 +1,31 @@
-include(FetchContent)
-include(ExternalProject)
-include(CPM)
-
-CPMAddPackage(
-    NAME matplotplusplus
-    GITHUB_REPOSITORY alandefreitas/matplotplusplus
-    GIT_TAG origin/master # or whatever tag you want
-    CMAKE_ARGS
-
-
-)
-
-# patch fix missing include file!
-add_custom_target(copy-files-matplot ALL
-   COMMAND ${CMAKE_COMMAND} -E copy_if_different
-   ${CMAKE_BINARY_DIR}/_deps/matplotplusplus-build/source/matplot/matplot/detail/exports.h
-   ${CMAKE_BINARY_DIR}/_deps/matplotplusplus-src/source/matplot/detail/exports.h
-   )
-
-
-# TODO: hack needed to link correctly! How can this be fixed?
-add_library(matplot_imp SHARED IMPORTED)
-add_dependencies(matplot_imp matplotplusplus copy-files-matplot)
-#set(MATPLOT_INCLUDE_DIRECTORY "${CMAKE_BINARY_DIR}/matplot/include")
-#set(MATPLOT_LIB_PATH "${CMAKE_BINARY_DIR}/matplot/lib/libmatplot.so")
-list(APPEND MATPLOT_INCLUDE_DIR
-  ${CMAKE_BINARY_DIR}/_deps/matplotplusplus-src/source
+if (NOT TARGET matplot)
+ include(ExternalProject)
+ ExternalProject_Add(matplot-ext
+   PREFIX ${CMAKE_BINARY_DIR}/matplot
+   GIT_REPOSITORY https://github.com/alandefreitas/matplotplusplus
+   GIT_TAG origin/master
+   UPDATE_COMMAND ""
+   #PATCH_COMMAND patch -p0 < ${CMAKE_SOURCE_DIR}/extra_version.patch
+   CMAKE_ARGS
+   -DBUILD_SHARED_LIBS=ON
+   -DMATPLOTPP_BUILD_INSTALLER=ON
+   -DCMAKE_INSTALL_PREFIX=${CMAKE_BINARY_DIR}/matplot
+   -DCMAKE_BUILD_TYPE=Release
+   -DMATPLOTPP_BUILD_EXAMPLES=OFF
+   -DMATPLOTPP_BUILD_TESTS=OFF
+   -DMATPLOTPP_BUILD_SHARED_LIBS=ON
+   -DMATPLOTPP_BUILD_WITH_SANITIZERS=OFF
  )
+ add_library(matplot SHARED IMPORTED GLOBAL)
+ add_dependencies(matplot matplot-ext)
+ file(MAKE_DIRECTORY ${CMAKE_BINARY_DIR}/matplot/include)
+ set_property(TARGET matplot PROPERTY INTERFACE_INCLUDE_DIRECTORIES
+     ${CMAKE_BINARY_DIR}/matplot/include
+     )
 
-if(CMAKE_BUILD_TYPE STREQUAL "Debug")
-  set_target_properties(matplot_imp PROPERTIES
-    IMPORTED_LOCATION ${CMAKE_BINARY_DIR}/libmatplotd.so
-    INTERFACE_INCLUDE_DIRECTORIES "${MATPLOT_INCLUDE_DIR}"
-  )
-else()
- set_target_properties(matplot_imp PROPERTIES
-   IMPORTED_LOCATION ${CMAKE_BINARY_DIR}/libmatplot.so
-   INTERFACE_INCLUDE_DIRECTORIES "${MATPLOT_INCLUDE_DIR}"
- )
+   set_target_properties(matplot PROPERTIES
+     IMPORTED_LOCATION ${CMAKE_BINARY_DIR}/matplot/lib/libmatplot.so)
+
 endif()
+
 
