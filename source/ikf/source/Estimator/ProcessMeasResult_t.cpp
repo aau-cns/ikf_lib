ccd91203b9686b21f630584727f5a1e5c11d1115
rojung <roland.jung@aau.at>
2023-09-09 09:00:35 +0200

    [ADD] to_string(const eMeasStatus s)  added

---
+++ source/ikf/source/Estimator/ProcessMeasResult_t.cpp
@@ -0,0 +1,34 @@
+/******************************************************************************
+ * FILENAME:     ProcessMeasResult_t.cpp
+ * PURPOSE:      %{Cpp:License:ClassName}
+ * AUTHOR:       jungr
+ * MAIL:         roland.jung@ieee.org
+ * VERSION:      v0.0.1
+ * CREATION:     08.09.2023
+ *
+ *  Copyright (C) 2023
+ *  All rights reserved. See the LICENSE file for details.
+ ******************************************************************************/
+#include <ikf/Estimator/ProcessMeasResult_t.hpp>
+
+namespace ikf {
+
+std::string to_string(const eMeasStatus s) {
+  switch (s) {
+  case eMeasStatus::REJECTED:
+    return "REJECTED";
+  case eMeasStatus::PROCESSED:
+    return "PROCESSED";
+  case eMeasStatus::OUTOFORDER:
+    return "OUTOFORDER";
+  case eMeasStatus::DISCARED:
+    return "DISCARED";
+  default:
+    return "";
+    break;
+  }
+}
+
+ProcessMeasResult_t::ProcessMeasResult_t(const eMeasStatus &s) : status(s) {}
+
+}  // namespace ikf
