d7a2db3300cd40f44a6657e2d6f9cdc991e7dc12
rojung <roland.jung@aau.at>
2024-07-11 17:48:04 +0200

    [FEAT] Timestamp::sec changed to INT64_T

--- source/ikf/include/ikf/Container/Timestamp.hpp
+++ source/ikf/include/ikf/Container/Timestamp.hpp
@@ -1,21 +1,21 @@
 /******************************************************************************
-* FILENAME:     Timestamp.hpp
-* PURPOSE:      Part of the ikf_lib
-* AUTHOR:       Roland Jung
-* MAIL:         jungr-ait@github
-* VERSION:      v0.0.0
-* CREATION:     19.01.2023
-*
-* Copyright (C) 2023 Roland Jung, Control of Networked Systems, University of Klagenfurt, Austria.
-*
-* All rights reserved.
-*
-* This software is licensed under the terms of the BSD-2-Clause-License with
-* no commercial use allowed, the full terms of which are made available
-* in the LICENSE file. No license in patents is granted.
-*
-* You can contact the author at <roland.jung@aau.at>
-******************************************************************************/
+ * FILENAME:     Timestamp.hpp
+ * PURPOSE:      Part of the ikf_lib
+ * AUTHOR:       Roland Jung
+ * MAIL:         jungr-ait@github
+ * VERSION:      v0.0.0
+ * CREATION:     19.01.2023
+ *
+ * Copyright (C) 2023 Roland Jung, Control of Networked Systems, University of Klagenfurt, Austria.
+ *
+ * All rights reserved.
+ *
+ * This software is licensed under the terms of the BSD-2-Clause-License with
+ * no commercial use allowed, the full terms of which are made available
+ * in the LICENSE file. No license in patents is granted.
+ *
+ * You can contact the author at <roland.jung@aau.at>
+ ******************************************************************************/
 #ifndef IKF_TIMESTAMP_HPP
 #define IKF_TIMESTAMP_HPP
 
@@ -23,66 +23,76 @@
 #include <ikf/ikf_api.h>
 #include <ostream>
 
-namespace ikf
-{
+namespace ikf {
 ///
 /// \brief The Timestamp class
 /// Q1: Why not using a floating point represtation?
-/// A1: Floats do not support a unique data representation (huge range dynamics, rounding and machine precission might lead to data comparision issues). The current implementation allows a discretization up to nanoseconds (should be sufficient for most applications) and is inspired by ROS.
-  class IKF_API Timestamp
-  {
-    public:
-      std::int32_t sec = 0;
-      std::int32_t nsec = 0;
+/// A1: Floats do not support a unique data representation (huge range dynamics, rounding and machine precission might
+/// lead to data comparision issues). The current implementation allows a discretization up to nanoseconds (should be
+/// sufficient for most applications) and is inspired by ROS.
 
-      ///
-      /// \brief Timestamp: CTORs
-      ///
-      Timestamp();
-      Timestamp(std::int32_t sec_, std::int32_t nsec_);
-      Timestamp(double const t_);
-      Timestamp(std::int64_t const stamp_ns);
-      virtual ~Timestamp();
+/**
+ * @brief The Timestamp class
+ * The current time is roughly 1720712106 seconds since the Epoch (1970-01-01 00:00 UTC) which requires already
+ * 31-bit. Since we use a signed integer, this time would fit in INT32_t variable, but leaves no headroom for numerical
+ * operations (adding two of them to compute the average causes an overflow!). Therefore the seconds are represented as
+ * INT64_t.
+ */
+class IKF_API Timestamp {
+public:
+  std::int64_t sec = 0;
+  std::int32_t nsec = 0;
 
-      double to_sec() const;
-      void from_sec(double const t = 0.0);
-      bool is_zero() const;
+  ///
+  /// \brief Timestamp: CTORs
+  ///
+  Timestamp();
+  Timestamp(std::int64_t sec_, std::int32_t nsec_);
+  Timestamp(double const t_);
+  Timestamp(std::int64_t const stamp_ns);
+  virtual ~Timestamp();
 
-      std::int64_t stamp_ns() const;
-      void from_stamp_ns(std::int64_t const stamp_ns = 0);
+  double to_sec() const;
+  void from_sec(double const t = 0.0);
+  bool is_zero() const;
 
-      std::string str() const;
+  std::int64_t stamp_ms() const;
+  void from_stamp_ms(std::int64_t const stamp_ms = 0);
+  std::int64_t stamp_us() const;
+  void from_stamp_us(std::int64_t const stamp_us = 0);
+  std::int64_t stamp_ns() const;
+  void from_stamp_ns(std::int64_t const stamp_ns = 0);
 
-      friend std::ostream& operator<<(std::ostream& out, const Timestamp& obj) {
-        out << obj.str();
-        return out;
-      }
+  std::string str() const;
 
-      bool operator<(Timestamp const& rhs) const;
-      bool operator==(Timestamp const& rhs) const;
-      bool operator<=(Timestamp const& rhs) const;
-      bool operator>(Timestamp const& rhs) const;
-      Timestamp operator+(const Timestamp& rhs) const { return Timestamp(to_sec() + rhs.to_sec()); }
-      Timestamp operator-(const Timestamp& rhs) const { return Timestamp(to_sec() - rhs.to_sec()); }
-      Timestamp& operator-=(const Timestamp& rhs);
-      Timestamp& operator+=(const Timestamp& rhs);
-  };
+  friend std::ostream& operator<<(std::ostream& out, const Timestamp& obj) {
+    out << obj.str();
+    return out;
+  }
 
-  class IKF_API Timestamped: public Timestamp
-  {
-    public:
-      virtual ~Timestamped();
-      virtual ikf::Timestamp get_timestamp();
+  bool operator<(Timestamp const& rhs) const;
+  bool operator==(Timestamp const& rhs) const;
+  bool operator<=(Timestamp const& rhs) const;
+  bool operator>(Timestamp const& rhs) const;
+  Timestamp operator+(const Timestamp& rhs) const;
+  Timestamp operator-(const Timestamp& rhs) const;
+  Timestamp& operator-=(const Timestamp& rhs);
+  Timestamp& operator+=(const Timestamp& rhs);
+};
 
-      bool operator < (Timestamped const& rhs) const;
+class IKF_API Timestamped : public Timestamp {
+public:
+  virtual ~Timestamped();
+  virtual ikf::Timestamp get_timestamp();
 
-      bool operator==(Timestamped const& rhs) const;
+  bool operator<(Timestamped const& rhs) const;
 
-      bool operator > (Timestamped const& rhs) const;
+  bool operator==(Timestamped const& rhs) const;
 
+  bool operator>(Timestamped const& rhs) const;
 
-  }; // Timestamped
+};  // Timestamped
 
-} // namespace ikf
+}  // namespace ikf
 
-#endif // IKF_TIMESTAMP_HPP
+#endif  // IKF_TIMESTAMP_HPP
--- source/tests/ikf-test/ikf_Timestamp_test.cpp
+++ source/tests/ikf-test/ikf_Timestamp_test.cpp
@@ -43,31 +43,225 @@ TEST_F(IKF_Timestamp_test, ctor_init)
     EXPECT_TRUE(t.sec == 1);
   }
   {
+    ikf::Timestamp t(0.000000001);
+    EXPECT_TRUE(t.nsec == 1);
+    EXPECT_TRUE(t.sec == 0);
+  }
+  {
     ikf::Timestamp t(1.0000000001);
-    EXPECT_TRUE(t.nsec == 0);
-    EXPECT_TRUE(t.sec == 1);
+    EXPECT_EQ(t.nsec, 0);
+    EXPECT_EQ(t.sec, 1);
   }
   {
     ikf::Timestamp t(1.000000000999);
-    EXPECT_TRUE(t.nsec == 1);
-    EXPECT_TRUE(t.sec == 1);
+    EXPECT_EQ(t.nsec, 1);
+    EXPECT_EQ(t.sec, 1);
   }
   {
     ikf::Timestamp t(1,1);
-    EXPECT_TRUE(t.nsec == 1);
-    EXPECT_TRUE(t.sec == 1);
+    EXPECT_EQ(t.nsec, 1);
+    EXPECT_EQ(t.sec, 1);
   }
+  {
+    ikf::Timestamp t(1.999999999);
+    EXPECT_EQ(t.nsec, 999999999);
+    EXPECT_EQ(t.sec, 1);
+  }
+  {
+    ikf::Timestamp t(8321499136.0);
+    EXPECT_EQ(t.nsec, 0);
+    EXPECT_EQ(t.sec, __INT64_C(0x1F0000000));
+  }
+}
+TEST_F(IKF_Timestamp_test, from_pos) {
   {
     ikf::Timestamp t(1.000000001);
     std::int64_t stamp_ns = t.stamp_ns();
     std::cout << "stamp_ns = " <<  t.stamp_ns() << std::endl;
-    EXPECT_TRUE(stamp_ns == 1000000001);
+    EXPECT_EQ(stamp_ns, 1000000001);
   }
   {
     std::int64_t stamp_ns = 1000000001;
     ikf::Timestamp t(stamp_ns);
-    EXPECT_TRUE(t.nsec == 1);
-    EXPECT_TRUE(t.sec == 1);
+    EXPECT_EQ(t.nsec, 1);
+    EXPECT_EQ(t.sec, 1);
+  }
+  {
+    ikf::Timestamp t(1.000001);
+    std::int64_t stamp_us = t.stamp_us();
+    std::cout << "stamp_us = " << t.stamp_us() << std::endl;
+    EXPECT_EQ(stamp_us, 1000001);
+  }
+  {
+    std::int64_t stamp_us = 1000001;
+    ikf::Timestamp t;
+    t.from_stamp_us(stamp_us);
+    EXPECT_EQ(t.nsec, 1000);
+    EXPECT_EQ(t.sec, 1);
+  }
+  {
+    ikf::Timestamp t(1.001);
+    std::int64_t stamp_ms = t.stamp_ms();
+    std::cout << "stamp_ms = " << t.stamp_ms() << std::endl;
+    EXPECT_EQ(stamp_ms, 1001);
+  }
+  {
+    std::int64_t stamp_ms = 1001;
+    ikf::Timestamp t;
+    t.from_stamp_ms(stamp_ms);
+    EXPECT_EQ(t.nsec, 1000000);
+    EXPECT_EQ(t.sec, 1);
+  }
+}
+
+TEST_F(IKF_Timestamp_test, from_neg) {
+  {
+    ikf::Timestamp t(-1.000000001);
+    std::int64_t stamp_ns = t.stamp_ns();
+    std::cout << "stamp_ns = " << t.stamp_ns() << std::endl;
+    EXPECT_EQ(stamp_ns, -1000000001);
+  }
+  {
+    std::int64_t stamp_ns = -1000000001;
+    ikf::Timestamp t(stamp_ns);
+    EXPECT_EQ(t.nsec, -1);
+    EXPECT_EQ(t.sec, -1);
+  }
+  {
+    ikf::Timestamp t(-1.000001);
+    std::int64_t stamp_us = t.stamp_us();
+    std::cout << "stamp_us = " << t.stamp_us() << std::endl;
+    EXPECT_EQ(stamp_us, -1000001);
+  }
+  {
+    std::int64_t stamp_us = -1000001;
+    ikf::Timestamp t;
+    t.from_stamp_us(stamp_us);
+    EXPECT_EQ(t.nsec, -1000);
+    EXPECT_EQ(t.sec, -1);
+  }
+  {
+    ikf::Timestamp t(-1.001);
+    std::int64_t stamp_ms = t.stamp_ms();
+    std::cout << "stamp_ms = " << t.stamp_ms() << std::endl;
+    EXPECT_EQ(stamp_ms, -1001);
+  }
+  {
+    std::int64_t stamp_ms = -1001;
+    ikf::Timestamp t;
+    t.from_stamp_ms(stamp_ms);
+    EXPECT_EQ(t.nsec, -1000000);
+    EXPECT_EQ(t.sec, -1);
+  }
+}
+
+TEST_F(IKF_Timestamp_test, from_neg_below_zero) {
+  {
+    ikf::Timestamp t(-0.000000001);
+    std::int64_t stamp_ns = t.stamp_ns();
+    std::cout << "stamp_ns = " << t.stamp_ns() << std::endl;
+    EXPECT_EQ(stamp_ns, -1);
+  }
+  {
+    std::int64_t stamp_ns = -0000000001;
+    ikf::Timestamp t(stamp_ns);
+    EXPECT_EQ(t.nsec, -1);
+    EXPECT_EQ(t.sec, 0);
+  }
+  {
+    ikf::Timestamp t(-0.000001);
+    std::int64_t stamp_us = t.stamp_us();
+    std::cout << "stamp_us = " << t.stamp_us() << std::endl;
+    EXPECT_EQ(stamp_us, -0000001);
+  }
+  {
+    std::int64_t stamp_us = -1;
+    ikf::Timestamp t;
+    t.from_stamp_us(stamp_us);
+    EXPECT_EQ(t.nsec, -1000);
+    EXPECT_EQ(t.sec, 0);
+  }
+  {
+    ikf::Timestamp t(-0.001);
+    std::int64_t stamp_ms = t.stamp_ms();
+    std::cout << "stamp_ms = " << t.stamp_ms() << std::endl;
+    EXPECT_EQ(stamp_ms, -1);
+  }
+  {
+    std::int64_t stamp_ms = -1;
+    ikf::Timestamp t;
+    t.from_stamp_ms(stamp_ms);
+    EXPECT_EQ(t.nsec, -1000000);
+    EXPECT_EQ(t.sec, 0);
+  }
+}
+TEST_F(IKF_Timestamp_test, add) {
+  {
+    ikf::Timestamp t1(1.000000001);
+    ikf::Timestamp t2(1.000000001);
+
+    auto t = t1 + t2;
+    std::int64_t stamp_ns = t.stamp_ns();
+    EXPECT_EQ(stamp_ns, 2000000002);
+  }
+  {
+    ikf::Timestamp t1(1.000000001);
+    ikf::Timestamp t2(-1.000000001);
+
+    auto t = t1 + t2;
+    std::int64_t stamp_ns = t.stamp_ns();
+    EXPECT_EQ(stamp_ns, 0);
   }
 
+  {
+    ikf::Timestamp t1(1.000000001);
+    ikf::Timestamp t2(-0.000000001);
+
+    auto t = t1 + t2;
+    std::int64_t stamp_ns = t.stamp_ns();
+    EXPECT_EQ(stamp_ns, 1000000000);
+  }
+  {
+    ikf::Timestamp t1(-1.000000001);
+    ikf::Timestamp t2(0.000000001);
+
+    auto t = t1 + t2;
+    std::int64_t stamp_ns = t.stamp_ns();
+    EXPECT_EQ(stamp_ns, -1000000000);
+  }
+}
+TEST_F(IKF_Timestamp_test, sub) {
+  {
+    ikf::Timestamp t1(-1.000000001);
+    ikf::Timestamp t2(1.000000001);
+
+    auto t = t1 - t2;
+    std::int64_t stamp_ns = t.stamp_ns();
+    EXPECT_EQ(stamp_ns, -2000000002);
+  }
+  {
+    ikf::Timestamp t1(1.000000001);
+    ikf::Timestamp t2(1.000000001);
+
+    auto t = t1 - t2;
+    std::int64_t stamp_ns = t.stamp_ns();
+    EXPECT_EQ(stamp_ns, 0);
+  }
+
+  {
+    ikf::Timestamp t1(1.000000001);
+    ikf::Timestamp t2(0.000000001);
+
+    auto t = t1 - t2;
+    std::int64_t stamp_ns = t.stamp_ns();
+    EXPECT_EQ(stamp_ns, 1000000000);
+  }
+  {
+    ikf::Timestamp t1(1.000000001);
+    ikf::Timestamp t2(-0.000000001);
+
+    auto t = t1 - t2;
+    std::int64_t stamp_ns = t.stamp_ns();
+    EXPECT_EQ(stamp_ns, 1000000002);
+  }
 }
